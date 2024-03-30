b98fb315a20fa2fe65f1712d6e59e442f90ea269
Tyler Wu <tylerwu2222@gmail.com>
2024-03-29 20:44:54 -0700

    toggleable pinyin functionality

--- src/components/Modules/HanziPinyinBlock/HanziPinyinBlock.js
+++ src/components/Modules/HanziPinyinBlock/HanziPinyinBlock.js
@@ -1,5 +1,7 @@
-import { View, Text } from 'react-native'
-import React from 'react';
+import { View, Text, Pressable } from 'react-native'
+import React, { useState, useContext } from 'react';
+import { AppContext } from '../../../../App';
+
 
 import { getPinYinText } from '../../../scripts/promptGetter';
 
@@ -10,6 +12,8 @@ import ChineseText from '../ChineseText/ChineseText';
 
 
 export default function HanziPinyinBlock({ hanziCharacter, customPinyinSize = null, customHanziSize = null }) {
+  const { showPinyin } = useContext(AppContext);
+  const [singlePinyin, setSinglePinyin] = useState(showPinyin);
   const pinyinSize = customPinyinSize ? customPinyinSize : 14;
 
 
@@ -22,13 +26,31 @@ export default function HanziPinyinBlock({ hanziCharacter, customPinyinSize = nu
       // textAlign: "center"
     },
     pinyinText: {
-      fontSize: pinyinSize
+      fontSize: pinyinSize,
+      opacity: showPinyin || singlePinyin ? 1 : 0,
+      pointerEvents: showPinyin || singlePinyin ? 'auto' : 'none',
+      // display: showPinyin || singlePinyin ? 'default' : 'none'
     }
   });
+
+  const readSinglePinyin = () => {
+    console.log('long pressed');
+  };
+
+  const toggleSinglePinyin = () => {
+    // console.log('toggle single PY', singlePinyin);
+    setSinglePinyin(!singlePinyin);
+  };
+
   return (
     <View style={styles.HZPYBlock}>
       <Text style={styles.pinyinText}>{getPinYinText(hanziCharacter)}</Text>
-      <ChineseText chineseText={hanziCharacter} />
+      <Pressable
+        onPress={() => { toggleSinglePinyin() }}
+        onLongPress={() => { readSinglePinyin() }}
+      >
+        <ChineseText chineseText={hanziCharacter} />
+      </Pressable>
     </View>
   )
 }
